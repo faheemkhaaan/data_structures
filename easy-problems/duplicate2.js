8ff207e6d0a009ee9feef253c8a166c5b5dd213a
faheemkhaaan <faheemkhan.pg@gmail.com>
2026-04-24 08:45:34 +0500

    1. solved leetcode problem containsNearbyDuplicate using sliding window.

---
+++ easy-problems/duplicate2.js
@@ -0,0 +1,41 @@
+
+const generator = require("../utils/array.js")
+
+function containsDuplicate2(array, k) {
+
+    if (k === 0) return false;
+
+    let start = 0;
+    const set = new Set();
+
+    for (let end = 0; end < array.length; end++) {
+
+        if (set.has(array[end])) {
+            return true
+        }
+        if (end - start >= k) {
+            set.delete(array[start]);
+            start++
+        }
+        set.add(array[end]);
+    }
+    console.log(set);
+    return false;
+}
+const map = new Map();
+
+map.set(2, 3);
+map.delete(2)
+
+const set = new Set();
+set.add(4);
+set.delete(4)
+set.has(4)
+
+const array = generator.randomSortedArray();
+const array2 = [2, 3, 4, 5, 6, 7, 8, 6];
+const k = 4;
+
+const result = containsDuplicate2(array, k);
+
+console.log(result);

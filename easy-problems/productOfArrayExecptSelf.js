1151bf3f1000a9b11b13c1f6694c869f5373b12c
faheemkhaaan <faheemkhan.pg@gmail.com>
2026-04-25 13:32:38 +0500

    1. optimized version of the productExceptSelf leet code problem with O(n) time complexity

--- easy-problems/productOfArrayExecptSelf.js
+++ easy-problems/productOfArrayExecptSelf.js
@@ -4,47 +4,47 @@
  */
 var productExceptSelf = function (nums) {
 
+    const n = nums.length;
+    const prefix = Array(n);
+    const suffix = Array(n);
+    const output = Array(n);
 
-    const output = [...nums];
-    const prefix = (index) => {
-        if (index === null) return 1;
-        let result = 1
-        for (let i = index - 1; i > 0; i--) {
-            result *= nums[i]
-            console.log({ name: "prefix", num: nums[i], result, index })
-        }
-        return result + 0;
-    }
-    const sufix = (index) => {
-        if (index === null) return 1;
-        let result = 1;
-        for (let i = index + 1; i < nums.length; i++) {
-            result *= nums[i];
-            console.log({ name: "sufix", num: nums[i], result, index })
-        }
-        return result + 0;
+    prefix[0] = 1;
+    for (let i = 1; i < n; i++) {
+        prefix[i] = prefix[i - 1] * nums[i - 1];
     }
 
-    for (let i = 0; i < nums.length; i++) {
+    suffix[n - 1] = 1;
+    for (let i = n - 2; i >= 0; i--) {
+        suffix[i] = suffix[i + 1] * nums[i + 1]
 
-
-        const pre = prefix(i);
-        const su = sufix(i);
-        console.log({ pre, su })
-        const result = su * pre
-        output[i] = result;
     }
+
+    for (let i = 0; i < n; i++) {
+        output[i] = prefix[i] * suffix[i]
+    }
+
     return output
+
 };
 
 const nums = [-1, 1, 0, -3, 3]
 const output = productExceptSelf(nums);
 /*
 
-    (-1)                    (1)              (0)            (-3)            (3)
-     |                       |                |
-    / \                     / \              / \
-  nul  (1)(0)(-3)(3)    (-1) (0)(-3)(3) (-1)(0) (-3)(3)
+  [-1,1,0,-3,3]
+  prefix = [1,-1,-1,0,0]
+  suffix = [0,0,-9,3,1]
+  output = [0,0,-9,0,0]
 */
 
 console.log(output)
+
+/*
+
+To calculate prefix.
+
+
+1. get the previous index value and if it is null we save the current index value in the calculated values.
+2. and if the previous index is not null we take the previous value and get the calcaulted value of previosu index multiply them and save those value.
+*/

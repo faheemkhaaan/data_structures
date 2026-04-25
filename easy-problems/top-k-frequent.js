258a710de39c1e08d4877ec03b00db62450680fb
faheemkhaaan <faheemkhan.pg@gmail.com>
2026-04-26 00:01:54 +0500

    1. solved the top k frequent elements in array problem.

---
+++ easy-problems/top-k-frequent.js
@@ -0,0 +1,123 @@
+
+
+
+function topKFrequentElements(nums, k) {
+    const result = new Map();
+    const queue = new PriorityQueue((a, b) => { return b.value - a.value });
+
+    for (let i = 0; i < nums.length; i++) {
+        if (result.has(nums[i])) {
+            result.set(nums[i], result.get(nums[i]) + 1);
+        } else {
+            result.set(nums[i], 1)
+        }
+    }
+
+    for (const [key, value] of result.entries()) {
+        queue.enqueue({ key, value });
+        if (queue.size() > k) {
+            queue.dequeue();
+        }
+    }
+    return queue.array.map(a => a.key);
+}
+
+class PriorityQueue {
+
+    constructor(compare) {
+        this.array = [];
+        this.compare = compare;
+    }
+
+    size() {
+        return this.array.length;
+    }
+    toArray() {
+        return [...this.array]
+    }
+
+    parent(index) {
+        return Math.floor((index - 1) / 2)
+    }
+
+    left(index) {
+        return (2 * index) + 1;
+    }
+
+    right(index) {
+        return (2 * index) + 2;
+    }
+
+    swap(i, j) {
+        [this.array[i], this.array[j]] = [this.array[j], this.array[i]];
+    }
+    getMax() {
+        return this.array[0]
+    }
+
+    enqueue(value) {
+
+        this.array.push(value);
+        this.bubbleUp(this.array.length - 1);
+
+    }
+
+    bubbleUp(index) {
+
+        while (index > 0 && this.compare(this.array[this.parent(index)], this.array[index]) > 0) {
+            this.swap(this.parent(index), index);
+            index = this.parent(index);
+        }
+    }
+    dequeue() {
+        let arr = this.array
+        if (this.array.length === 1) {
+            return arr.pop()
+        };
+
+        let max = arr[0];
+        this.array[0] = this.array.pop();
+        this.sinkDown(0);
+        return max;
+    }
+    sinkDown(index) {
+        let leftChildIndex = this.left(index);
+        let rightChildIndex = this.right(index);
+        const n = this.array.length;
+
+        let largest = index;
+
+        if (leftChildIndex < n && this.compare(this.array[largest], this.array[leftChildIndex]) > 0) {
+            largest = leftChildIndex;
+        }
+        if (rightChildIndex < n && this.compare(this.array[largest], this.array[rightChildIndex]) > 0) {
+            largest = rightChildIndex;
+        }
+
+        if (largest !== index) {
+            this.swap(index, largest);
+            this.sinkDown(largest);
+        }
+    }
+}
+
+function heapSort(nums) {
+    const heap = new PriorityQueue((a, b) => b - a);
+
+    for (const num of nums) {
+        heap.enqueue(num);
+    }
+    const sorted = [];
+
+    while (heap.size() > 0) {
+        sorted.push(heap.dequeue());
+    }
+    return sorted
+}
+
+const arr = [4, 2, 63, 2, 34, 5, 43, 6, 6, 7, 6, 8, 4, 3, 53, 4, 34, 53, 2];
+const sorted = heapSort(arr);
+console.log(sorted);
+const result = topKFrequentElements(arr, 3);
+
+// console.log(result);

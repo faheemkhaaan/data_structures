ef5bcbc1c6ff5e0daa323a92b4c603a2575252f8
faheemkhaaan <faheemkhan.pg@gmail.com>
2026-06-03 23:21:27 +0500

    - solving longest common prefix problem from leet code

---
+++ easy-problems/longest-commont-prefix.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+
+    let keep = strs[i];
+    for (let i = 1; i < strs.length; i++) {
+
+
+
+    }
+};
+
+const input = ["flower", "flow", "flight"];
+
+const result = longestCommonPrefix(input);

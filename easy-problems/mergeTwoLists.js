b0b3408766f8fb6cb343e4bba0db6ad9374f3558
faheemkhaaan <faheemkhan.pg@gmail.com>
2026-06-05 23:52:01 +0500

    - first try to solve this problem.

--- easy-problems/mergeTwoLists.js
+++ easy-problems/mergeTwoLists.js
@@ -1,9 +1,9 @@
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val)
+    this.next = (next === undefined ? null : next)
+}
 /**
  * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
  */
 /**
  * @param {ListNode} list1
@@ -12,8 +12,40 @@
  */
 var mergeTwoLists = function (list1, list2) {
 
+
+    const result = new ListNode();
+
+    let p1 = list1.val;
+    let p2 = list2.val;
+
+    if (p1 > p2) {
+        result.val = p1;
+        result.next = new ListNode(p2);
+    } else {
+        result.val = p2;
+        result.next = new ListNode(p1);
+    }
+
+    while (list1.next || list2.next) {
+        p1 = list1.next.val;
+        p2 = list2.next.val;
+
+        if (p1 > p2) {
+            result.val = p1;
+            result.next = new ListNode(p2);
+        } else {
+            result.val = p2;
+            result.next = new ListNode(p1);
+        }
+        list1.next = list1.next.next;
+        list2.next = list2.next.next;
+    }
+    return result
 };
 
-const list1 = [1, 2, 4];
-const list2 = [1, 3, 4]
+const list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+const list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+const result = mergeTwoLists(list1, list2);
+console.log(result);
 

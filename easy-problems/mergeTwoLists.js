2f545b3fb79156f12e39fd763253b2dd2556885c
faheemkhaaan <faheemkhan.pg@gmail.com>
2026-06-05 23:58:20 +0500

    - solved the problem

--- easy-problems/mergeTwoLists.js
+++ easy-problems/mergeTwoLists.js
@@ -2,50 +2,39 @@ function ListNode(val, next) {
     this.val = (val === undefined ? 0 : val)
     this.next = (next === undefined ? null : next)
 }
-/**
- * Definition for singly-linked list.
- */
+
 /**
  * @param {ListNode} list1
  * @param {ListNode} list2
  * @return {ListNode}
  */
 var mergeTwoLists = function (list1, list2) {
+    const dummy = new ListNode(-1);
+    let current = dummy;
 
-
-    const result = new ListNode();
-
-    let p1 = list1.val;
-    let p2 = list2.val;
-
-    if (p1 > p2) {
-        result.val = p1;
-        result.next = new ListNode(p2);
-    } else {
-        result.val = p2;
-        result.next = new ListNode(p1);
-    }
-
-    while (list1.next || list2.next) {
-        p1 = list1.next.val;
-        p2 = list2.next.val;
-
-        if (p1 > p2) {
-            result.val = p1;
-            result.next = new ListNode(p2);
+    while (list1 !== null && list2 !== null) {
+        if (list1.val <= list2.val) {
+            current.next = list1;
+            list1 = list1.next;
         } else {
-            result.val = p2;
-            result.next = new ListNode(p1);
+            current.next = list2;
+            list2 = list2.next;
         }
-        list1.next = list1.next.next;
-        list2.next = list2.next.next;
+        current = current.next;
     }
-    return result
+
+    if (list1 !== null) {
+        current.next = list1;
+    } else {
+        current.next = list2;
+    }
+
+    return dummy.next;
 };
 
 const list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
 const list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
 
 const result = mergeTwoLists(list1, list2);
-console.log(result);
+console.log(JSON.stringify(result));
 

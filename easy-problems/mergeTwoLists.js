f71bc1d4804ecdfddffff4b071e2e43da995153c
faheemkhaaan <faheemkhan.pg@gmail.com>
2026-06-05 23:33:38 +0500

    - started working on solving the mergeTwoLists leetcode problem.

---
+++ easy-problems/mergeTwoLists.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (list1, list2) {
+
+};
+
+const list1 = [1, 2, 4];
+const list2 = [1, 3, 4]
+

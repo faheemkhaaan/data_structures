8ad846fca925ea23ce13f01f6ddecddc199d4db0
faheemkhaaan <faheemkhan.pg@gmail.com>
2026-06-09 20:24:26 +0500

    - optimized the solution by count the word from the end and skiping the white spaces. with O(1) space complexity.

--- easy-problems/lengthOfLastWord.js
+++ easy-problems/lengthOfLastWord.js
@@ -4,15 +4,15 @@
  */
 var lengthOfLastWord = function (s) {
 
-
     let count = 0;
-    const st = s.trim()
-    for (let i = 0; i < st.length; i++) {
-        if (st[i].trim()) {
-            count++;
-        } else {
-            count = 0;
-        }
+    let i = s.length - 1;
+
+    while (i >= 0 && s[i] === " ") {
+        i--
+    }
+    while (i >= 0 && s[i] !== ' ') {
+        count++;
+        i--
     }
     return count;
 };

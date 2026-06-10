6bee1a4aa63024e2f3850849ab8b9244405a2dce
faheemkhaaan <faheemkhan.pg@gmail.com>
2026-06-10 21:49:27 +0500

    - finaly solved this problem - mergeSortedArray leetcode. probelm

--- easy-problems/mergeSortedArrays.js
+++ easy-problems/mergeSortedArrays.js
@@ -12,15 +12,14 @@ var merge = function (nums1, m, nums2, n) {
     let p1 = m - 1;
     let p2 = n - 1;
 
-    while (p > 0) {
-
-        const p1GreaterThanP2 = nums1[p1] >= nums2[p2]
+    while (p >= 0) {
+        const p1GreaterThanP2 = (nums1[p1] !== undefined ? nums1[p1] : -Infinity) >= (nums2[p2] !== undefined ? nums2[p2] : -Infinity);
         if (p1GreaterThanP2) {
             nums1[p] = nums1[p1];
-            p1--
+            --p1
         } else {
             nums1[p] = nums2[p2];
-            p2--
+            --p2
         }
         p--
     }
@@ -28,7 +27,7 @@ var merge = function (nums1, m, nums2, n) {
 };
 
 
-const nums1 = [1, 2, 3, 0, 0, 0], m = 3, nums2 = [2, 5, 6], n = 3;
+const nums1 = [-1, 0, 0, 3, 3, 3, 0, 0, 0], m = 6, nums2 = [1, 2, 2], n = 3;
 
 
 const result = merge(nums1, m, nums2, n);
